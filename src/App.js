f4fb039adbb3c1a6bbca225793e387ef3537ea3a
nbbosco <nickilu009@gmail.com>
2022-10-21 10:49:45 +0300

    NB_Web

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import background from './background.gif';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <img src={background} className="App-background" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
